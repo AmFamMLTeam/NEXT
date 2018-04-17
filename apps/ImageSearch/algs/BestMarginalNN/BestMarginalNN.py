7c0fbd2142380a728b14c2bb07d3a8a8f0c7c21a
Devin M Conathan <DCONATHA@amfam.com>
2018-04-17 15:22:33 -0500

    added best marginal alg

--- apps/ImageSearch/algs/BestMarginalNN/BestMarginalNN.py
+++ apps/ImageSearch/algs/BestMarginalNN/BestMarginalNN.py
@@ -2,16 +2,11 @@ import numpy as np
 
 from apps.ImageSearch.algs.NearestNeighbor.NearestNeighbor import NearestNeighbor
 from apps.ImageSearch.algs.utils import can_fit, get_X, sparse2list
-from apps.ImageSearch.algs.models import MarginalRegression
-from sklearn.linear_model import LogisticRegression
+from apps.ImageSearch.algs.models import BestMarginalRegression
 
-
-# TODO select best!
 
 class BestMarginalNN(NearestNeighbor):
     def initExp(self, butler, n, seed_i, alg_args):
-        N = alg_args[butler.alg_label].get('N', 1)
-        butler.algorithms.set(key='N', value=N)
         butler.algorithms.set(key='coefs', value=None)
         butler.algorithms.set(key='n_coefs', value=None)
         return super(BestMarginalNN, self).initExp(butler, n, seed_i, alg_args)
@@ -22,9 +17,8 @@ class BestMarginalNN(NearestNeighbor):
         y = [labels.get(k) for k in labeled]
         X = get_X(butler)
         if can_fit(y):
-            N = butler.algorithms.get(key='N')
-            model = MarginalRegression().fit(X[labeled], y)
-            mask = model.topfeatures(len(y) // N)
+            model = BestMarginalRegression().fit(X[labeled], y)
+            mask = model.topfeatures('best')
             coefs = model.coef_
             coefs[np.logical_not(mask)] = 0
             coefs = sparse2list(coefs)

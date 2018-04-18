1e7fbd27ab3769ec45b4077720b4e3963fde835b
Devin M Conathan <DCONATHA@amfam.com>
2018-04-18 14:22:35 -0500

    linear nearest neighbor (dot product?)

---
+++ apps/ImageSearch/algs/Linear/Linear.py
@@ -0,0 +1,57 @@
+import random
+import time
+
+import numpy as np
+
+from apps.ImageSearch.algs.base import BaseAlgorithm, QUEUE_SIZE
+from apps.ImageSearch.algs.utils import is_locked
+from next.utils import debug_print
+
+
+class Linear(BaseAlgorithm):
+    def fill_queue(self, butler, args):
+        if is_locked(butler.algorithms.memory.lock('fill_queue')):
+            debug_print('fill_queue is running already')
+            return
+        queue = butler.algorithms.get(key='queue')
+        if len(queue) > len(args['queue']):
+            debug_print('fill_queue called already')
+            return
+        with butler.algorithms.memory.lock('fill_queue'):
+            debug_print('filling queue')
+            X = self.select_features(butler, {})
+            d = X.shape[1]
+            labels = dict(butler.algorithms.get(key='labels'))
+            unlabeled = []
+            positives = []
+            n = butler.algorithms.get(key='n')
+            for i in xrange(n):
+                if i not in labels:
+                    unlabeled.append(i)
+                elif labels[i] == 1:
+                    positives.append(i)
+            target = random.choice(positives)
+            x = X[target]
+            a, b = np.polyfit([4096*2, 1], [10000, 424924], 1)
+            n_sample = int(a*d + b)
+            if len(unlabeled) > n_sample:
+                debug_print('sampling {} unlabeled examples'.format(n_sample))
+                unlabeled = random.sample(unlabeled, n_sample)
+            X = X[unlabeled]
+            debug_print('computing distances')
+            t0 = time.time()
+            dists = np.matmul(X, x)
+            debug_print('took {}s'.format(time.time() - t0))
+            dists = np.argsort(-dists)
+            queries = butler.algorithms.get(key='queries') - butler.algorithms.get(key='last_filled')
+            queue_size = max(QUEUE_SIZE, queries * 2)
+            self.set_queue(butler, [unlabeled[i] for i in dists[:queue_size]])
+            butler.algorithms.set(key='last_filled', value=butler.algorithms.get(key='queries'))
+
+    def constraint(self, butler):
+        """
+        This should return a function that is of the form:
+            f(n_coefficients: int) -> bool
+        That is, takes the number of features used by the model and returns whether that should be included as a possible model.
+        """
+        raise NotImplementedError
---
+++ apps/ImageSearch/algs/Linear/__init__.py
@@ -0,0 +1,1 @@
+from apps.ImageSearch.algs.Linear.Linear import Linear as MyAlg
